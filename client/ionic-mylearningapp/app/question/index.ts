969537580aae79f0292cdb709aa607582f0b72a8
Daniel Dobras <daniel.dobras@gmx.de>
2016-12-02 09:41:17 +0100

    redirecting to register

--- client/ionic-mylearningapp/app/question/index.ts
+++ client/ionic-mylearningapp/app/question/index.ts
@@ -2,3 +2,4 @@ export * from './createQuestion.component';
 export * from './listQuestions.component';
 export * from './deleteQuestion.component';
 export * from './editQuestion.component';
+export * from './manageQuestion.component';
---
+++ client/ionic-mylearningapp/app/question/manageQuestion.component.ts
@@ -0,0 +1,6 @@
+import {Component} from '@angular/core';
+
+@Component({
+  templateUrl: 'build/quesion/manageQuestion.component.html'
+})
+export class ManageQuestion {}
