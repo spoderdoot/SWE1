2af27b333f9ffb1e66573b5f2a840aec072acc3e
Fernando Francisco Pfennig <fernandopfennig@gmail.com>
2016-12-30 09:21:43 +0100

    fixes to question database

--- Server/src/QuestionDataSource.ts
+++ Server/src/QuestionDataSource.ts
@@ -58,7 +58,7 @@ export class QuestionDataSource {
                 questiondb.run("INSERT INTO Questions VALUES (8, 'Mathe', 'false', 'Was ist 7*8', NULL, NULL, NULL, NULL, '56');");
                 questiondb.run("INSERT INTO Questions VALUES (9, 'Mathe', 'true', 'Was ist Pii?', 'Eine ganze Zahl', 'Eine irrationale Zahl', 'Eine rationale Zahl', 'Eine natürliche Zahl', 'Eine irrationale Zahl');")
                 questiondb.run("INSERT INTO Questions VALUES (10, 'Mathe', 'true', 'Was ist 9*4?','31', '33', '35', '36', '36');");
-                questiondb.run("INSERT INTO Questions VALUES (11, 'Mathe', 'true', 'Was ist 8/2?','4', '3', '2', '1', '4');");
+                questiondb.run("INSERT INTO Questions VALUES (11, 'Mathe', 'true', 'Wie lautet der Satz des Pythagoras?','a²+b²=c²', 'a+b=c', 'a*b=c', 'a²-b²=c²', 'a²+b²=c²');");
                 questiondb.run("INSERT INTO Questions VALUES (12, 'Mathe', 'true', 'Was ist (9/3)*9+1?','31', '29', '30', '28', '28');");
                 questiondb.run("INSERT INTO Questions VALUES (13, 'Mathe', 'true', 'Was ist (4²/4²)?','1', '0', '2', '0,5', '1');");
                 questiondb.run("INSERT INTO Questions VALUES (14, 'Mathe', 'true', 'Was ist (44/2)+8?','31', '33', '30', '32', '30');");
